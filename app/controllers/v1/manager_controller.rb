e7cb6f1399da71bb651fcc76fb77e3f003c38c83
nicknish09 <nnmarques97@gmail.com>
2020-05-18 18:39:16 -0300

    atualizados specs de restaurants

--- app/controllers/v1/manager_controller.rb
+++ app/controllers/v1/manager_controller.rb
@@ -1,4 +1,13 @@
 module V1
   class ManagerController < ApiController
+
+    private
+
+    def check_if_owner
+      @restaurant = Restaurant.find_by(subdomain: requeset.subdomain)
+      unless @restaurant.user == current_user
+        render json: { error: 'Apenas o dono do restaurante tem acesso à isso' }, status: :unauthorized
+      end
+    end
   end
 end
