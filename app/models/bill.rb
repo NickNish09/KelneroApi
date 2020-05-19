34b2541cc59c80e98c45e6082976b46570d30b83
nicknish09 <nnmarques97@gmail.com>
2020-05-19 13:24:26 -0300

    adicionado user optional pra bill

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,6 +1,6 @@
 class Bill < ApplicationRecord
   belongs_to :table, optional: true
-  belongs_to :user
+  belongs_to :user, optional: true
 
   has_many :bill_items
   has_many :items, through: :bill_items
