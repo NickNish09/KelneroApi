077fa760c8d409881298f863046cc2302fb12853
nicknish09 <nnmarques97@gmail.com>
2020-05-21 20:22:33 -0300

    model de bill mostrando toda conta

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -6,4 +6,14 @@ class Bill < ApplicationRecord
   has_many :items, through: :bill_items
 
   validates :final_bill, presence: true
+
+  def as_json(options = {})
+    {
+      id: id,
+      final_bill: final_bill,
+      table: table,
+      user: user,
+      bill_items: bill_items,
+    }
+  end
 end
--- app/models/bill_item.rb
+++ app/models/bill_item.rb
@@ -1,4 +1,12 @@
 class BillItem < ApplicationRecord
   belongs_to :item
   belongs_to :bill
+
+  def as_json(options = {})
+    {
+        id: id,
+        item: item,
+        quantity: quantity
+    }
+  end
 end
