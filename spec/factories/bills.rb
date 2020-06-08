35871317b713c93cdcd676f341645e37413fb092
nicknish09 <nnmarques97@gmail.com>
2020-06-08 18:31:07 -0300

    test for total_bill_items

--- spec/factories/bills.rb
+++ spec/factories/bills.rb
@@ -3,5 +3,15 @@ FactoryBot.define do
     final_bill { 19.80 }
     table { create(:table) }
     user { create(:user) }
+
+    factory :bill_with_orders do
+      transient do
+        items_count { 10 }
+      end
+
+      after(:create) do |bill, evaluator|
+        create_list(:order, evaluator.items_count, bill: bill)
+      end
+    end
   end
 end
--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Table, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActionView::Helpers::NumberHelper
+
+  describe "#current_final_bill" do
+    before do
+      @table = create(:table)
+      create(:bill, final_bill: 10.00, table: @table)
+      create(:bill, final_bill: 20.50, table: @table)
+    end
+
+    it 'should return the sum of all the bills in the table' do
+      expect(@table.current_final_bill).to eq number_to_currency(30.50)
+    end
+  end
+
+  describe "#total_bill_items" do
+    before do
+      @table = create(:table)
+      create(:bill_with_orders, table: @table)
+      create(:bill_with_orders, table: @table)
+    end
+
+    it 'should return all the items in the bill from all bills' do
+      expect(@table.total_bill_items.count).to eq 20
+    end
+  end
 end
