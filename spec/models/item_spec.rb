e1adcdf4d91b45b25a70dc31428bee03249c180e
nicknish09 <nnmarques97@gmail.com>
2020-06-12 15:44:08 -0300

    testando funcao de pegar top selling items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,4 +10,29 @@ RSpec.describe Item, type: :model do
       expect(@item.image_name).to eq "popao_999_image"
     end
   end
+
+  describe "#self.top_selling_items" do
+    before do
+      @item_1 = create(:item, name: "Item 1")
+      @item_2 = create(:item, name: "Item 2")
+      @item_3 = create(:item, name: "Item 3")
+
+      3.times do # item 1 vai ser o top 1 de vendas
+        create(:order, item: @item_1)
+      end
+
+      2.times do # item 2 vai ser o top 2 de vendas
+        create(:order, item: @item_2)
+      end
+
+      1.times do # item 3 vai ser o top 3 de vendas
+        create(:order, item: @item_2)
+      end
+    end
+
+    it 'should return the list of the top selling items' do
+      expect(Item.top_selling_items.first).to eq @item_1
+      expect(Item.top_selling_items.last).to eq @item_3
+    end
+  end
 end
