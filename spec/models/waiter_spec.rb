ac76eb5f25d64271ae8fd37583f4961b1ca711c0
nicknish09 <nnmarques97@gmail.com>
2020-06-15 14:05:17 -0300

    testadas funcoes no model de waiter

--- spec/models/waiter_spec.rb
+++ spec/models/waiter_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Waiter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#set_auth_code" do
+    before do
+      @waiter = create(:waiter)
+    end
+
+    it 'should be run before a waiter is created to set his auth code' do
+      expect(@waiter.auth_code).to_not be_nil
+    end
+
+    it 'should generate a auth code with 6 digits' do
+      expect(@waiter.auth_code.length).to eq 6
+    end
+  end
+
+  describe "#set_token" do
+    before do
+      @waiter = create(:waiter)
+    end
+
+    it 'should generate a token to the waiter' do
+      expect(@waiter.token).to_not be_nil
+    end
+  end
 end
