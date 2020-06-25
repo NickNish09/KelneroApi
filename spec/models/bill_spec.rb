520ebc7738d9c228aa49a17e3df45b3547392339
nicknish09 <nnmarques97@gmail.com>
2020-06-25 15:25:50 -0300

    atualizado bill spec

--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Bill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#close_bill" do
+    before do
+      @bill = create(:bill)
+    end
+
+    it 'should set the current moment to closed_in attribute of the bill' do
+      @bill.close_bill
+      expect(@bill.closed_in).not_to be_nil
+    end
+  end
 end
